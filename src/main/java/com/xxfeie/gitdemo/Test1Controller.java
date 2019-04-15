208d58c9923f40872fbe5ad6d04c2260a5d8ac5f
reed_glb <reedwu_glb@meihua.info>
2019-04-15 15:29:58 +0800

    hello commit

--- src/main/java/com/xxfeie/gitdemo/Test1Controller.java
+++ src/main/java/com/xxfeie/gitdemo/Test1Controller.java
@@ -3,6 +3,6 @@ package com.xxfeie.gitdemo;
 public class Test1Controller {
     public static void main(String[] args)
     {
-        System.out.println("hello world");
+        System.out.println("hello world!!!");
     }
 }
